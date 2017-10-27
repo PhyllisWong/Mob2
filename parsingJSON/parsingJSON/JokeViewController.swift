b70842206f9a6f95b7c5a79e56b5908a75586c60
Phyllis Wong <wong.phyllism@gmail.com>
2017-10-27 14:39:42 -0700

    parsed json using decodable

--- parsingJSON/parsingJSON/JokeViewController.swift
+++ parsingJSON/parsingJSON/JokeViewController.swift
@@ -8,18 +8,19 @@
 
 import UIKit
 
-struct Joke {
+struct Joke : Decodable {
     let id: Int
     let type: String
     let setup: String
     let punchline: String
-
-    init(json: [String: Any]) {
-        id = json["id"] as? Int ?? -1
-        type = json["type"] as? String ?? ""
-        setup = json["setup"] as? String ?? ""
-        punchline = json["punchline"] as? String ?? ""
-    }
+    
+    // Swift 2/3/Obj ~~~ Do not need in Swift 4
+//    init(json: [String: Any]) {
+//        id = json["id"] as? Int ?? -1
+//        type = json["type"] as? String ?? ""
+//        setup = json["setup"] as? String ?? ""
+//        punchline = json["punchline"] as? String ?? ""
+//    }
 }
 
 class ViewController: UIViewController {
@@ -29,17 +30,19 @@ class ViewController: UIViewController {
         
         let jsonURLString = "https://08ad1pao69.execute-api.us-east-1.amazonaws.com/dev/random_joke"
         guard let url = URL(string: jsonURLString) else { return }
-        
         URLSession.shared.dataTask(with: url) { (data, response, err) in
             guard let data = data else { return }
             
             do {
-                guard let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? [String: Any] else { return }
-//                print(json)
-                let joke = Joke(json: json)
-                print(joke.punchline)
-                
+                let joke = try JSONDecoder().decode(Joke.self, from: data)
+                print(joke.id)
+                // Swift 2/3/ObjC style
+//                guard let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? [String: Any] else { return }
+////                print(json)
+//                let joke = Joke(json: json)
+//                print(joke.punchline)
             }
+                
             catch let jsonErr {
                 print("error serializing json:", jsonErr)
             }
