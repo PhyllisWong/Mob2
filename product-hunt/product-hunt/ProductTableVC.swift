b41c13dda9b01231c0a312b31cdfa24b48c383bb
Phyllis Wong <wong.phyllism@gmail.com>
2017-11-01 12:48:52 -0700

    Create data structure to display to TableViewVC

--- product-hunt/product-hunt/ProductTableVC.swift
+++ product-hunt/product-hunt/ProductTableVC.swift
@@ -8,8 +8,52 @@
 
 import UIKit
 
+// Data structure to display in each tableViewCell
+struct Post {
+    let commentsCount : Int
+    let name : String
+    let tagLine : String
+    let thumbnail : Thumbnail
+    let votesCount : Int
+}
 
+// Data structure to access the image URL
+struct Thumbnail: Decodable {
+    let imageURL : String
+}
 
+// Extend the Post struct to convert JSON to Swift naming convention
+extension Post: Decodable {
+    
+    enum PostKeys: String, CodingKey {
+        case commentsCount = "comments_count"
+        case name
+        case tagLine = "tag_line"
+        case thumbnail
+        case votesCount = "votes_count"
+    }
+    
+    enum ImageResultKey: String, CodingKey {
+        case imageURL = "image_url"
+    }
+   
+    init(from decoder: Decoder) throws {
+        // Defining the (keyed) container
+        let container = try decoder.container(keyedBy: PostKeys.self)
+        
+        let commentsCount: Int = try container.decodeIfPresent(Int.self, forKey: .commentsCount) ?? 0
+        let name: String = try container.decodeIfPresent(String.self, forKey: .name) ?? ""
+        let tagLine: String = try container.decodeIfPresent(String.self, forKey: .tagLine) ?? ""
+        let votesCount: Int = try container.decodeIfPresent(Int.self, forKey: .votesCount) ?? 0
+        
+        // thumbnail container
+        let thumbnailContainer = try container.nestedContainer(keyedBy: ImageResultKey.self, forKey: .thumbnail)
+        let thumbnail = try thumbnailContainer.decode(Thumbnail.self, forKey: .imageURL)
+
+        self.init(commentsCount: commentsCount, name: name, tagLine: tagLine, thumbnail: thumbnail, votesCount: votesCount)
+       
+    }
+}
 
 class ProductTableVC: UITableViewController {
     
