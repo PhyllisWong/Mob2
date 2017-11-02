e0076d47f23b4df0f1d5b2566489a4ddd252dc52
Phyllis Wong <wong.phyllism@gmail.com>
2017-11-01 20:27:34 -0700

    added comments

--- product-hunt/product-hunt/ProductTableVC.swift
+++ product-hunt/product-hunt/ProductTableVC.swift
@@ -8,24 +8,25 @@
 
 import UIKit
 
-
-
 class ProductTableVC: UITableViewController {
     
-    // Placeholder to format view cells
-    
+    // Empty array to hold the posts fetched from data
     var posts = [Post]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        // Instance of networking class
         let networking = Networking()
+        // calling the fetch callback on the instance of networking
         networking.fetchPost() { (posts) in
+            // Tell the async queue to bring the data back to the main thread
             DispatchQueue.main.async {
+                // populates the posts array in the main thread
                 self.posts = posts
+                // update the viewController with the data
                 self.tableView.reloadData()
             }
         }
-        
     }
     
     override func numberOfSections(in tableView: UITableView) -> Int {
@@ -34,7 +35,6 @@ class ProductTableVC: UITableViewController {
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return posts.count
     }
-    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "productCell", for: indexPath)
         cell.textLabel?.text = posts[indexPath.row].name
